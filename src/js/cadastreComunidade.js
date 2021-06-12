5d0287fe3006a90c624e150b7b0aca80b5eed186
Eric Mantoani <eu@eric.com.br>
2021-06-12 11:08:40 -0300

    integração com API para cadastro de comunidade

---
+++ src/js/cadastreComunidade.js
@@ -0,0 +1,52 @@
+// aqui estou importando uma classe javascript que transforma os dados de um formulário html em um objeto JSON
+// a instrução "import" só funciona se, no html, o tipo de script for declarado como "module"
+// exemplo: <script type="module" src="./js/cadastreComunidade.js"></script>
+import Form from './Form.js'
+import bsModal from './bsModal.js'
+
+let form = document.getElementById('cadastreComunidade');
+
+// criando listener para o evento "form submit"
+// o parâmetro "e" que eu recebo na função é o evento em si, que é um objeto com propriedades e métodos
+// é possível ver tudo o que um evento tem dando um console.log(e);
+form.addEventListener('submit', (e) => { 
+  // aqui estou usando dois métodos do evento que impedem o browser de seguir seu comportamento padrão para envio de formulários
+  // isso possibilita que a gente escreva o que quer fazer aqui dentro.
+  e.preventDefault(); e.stopPropagation();
+
+
+  let data = {
+    data: Form.data(form) // obtem o JSON dos dados do formulário e atribui a um objeto data, esperado pela API
+  }
+
+  fetch('http://api.wiki.local/wiki/comunidade/create', {
+    method: 'POST', // API espera envio por método POST
+    headers: {
+      'Content-type': 'application/json' // API espera receber conteúdo do tipo JSON
+    },
+    body: JSON.stringify(data) // é preciso transformar o objeto `data` em string para transitá-lo por HTTP.
+  }).then(r => r.json()) // como resultado, também volta uma string, é preciso então parseá-la como JSON
+    .then(r => { // e então usar
+
+      let id = 'modalCadastreComunidade'; 
+
+      // aqui criei um objeto `bsModal` para poder popular dinamicamente a partir do resultado da API.
+      // ver: bsModal.js importado no topo.
+      let Modal = new bsModal({
+        id: id,
+        title: 'Cadastre sua comunidade',
+        message: r.message
+      })
+
+      // renderizando dentro do elemento body, usando o objeto bsModal pra isso.
+      Modal.render(document.getElementsByTagName('body')[0]);
+
+      // aproveitei o modal que tinha sido feito pela Mariana, que é do bootstrap.
+      // então chamei o objeto Modal do bootstrap para manipulá-lo. Então mandei exibi-lo depois
+      // de o ter inserido no body html.
+      (new bootstrap.Modal(document.getElementById(id), {})).show();
+
+    })
+
+})
+
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,7 +6,7 @@ function random(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var Home = {
+var Header = {
 
 	mainImages: [
 		'https://pib.socioambiental.org/images/povos/76983097518bfcf79468b.jpg',
@@ -15,106 +15,19 @@ var Home = {
 	],
 
 	init: () => {
-		Home.mainImage();
+		Header.mainImage();
 	},
 
 	mainImage: () => {
-		document.getElementById('main-image').style.backgroundImage = `url('${Home.mainImages[random(0, 3)]}')`;
+		document.getElementById('main-image').style.backgroundImage = `url('${Header.mainImages[random(0, 3)]}')`;
 	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	Home.init();
-	clickResponse("nome");
+	Header.init();
 });
 
 
 
-var btnNome = document.getElementById('nome');
-btnNome.addEventListener('click', function () {
-	clickResponse('nome');
-}
-	, false);
 
-var btnLingua = document.getElementById('lingua');
-btnLingua.addEventListener('click', function () {
-	clickResponse('lingua');
-}
-	, false);
 
-var btnLocalidade = document.getElementById('localidade');
-btnLocalidade.addEventListener('click', function () {
-	clickResponse('localidade');
-}
-	, false);
-
-function clickResponse(titulo) {
-	var inicioLista = "<ul>"
-	var fimLista = "</ul>"
-	var divTela = document.getElementById('tribos');
-	
-	var lista = inicioLista;
-	
-	if (titulo == "nome") {
-		lista += imprimeNomes();		
-	}
-	else if (titulo == "lingua") {
-		lista += imprimeLingua();
-	}
-	else {
-		lista += imprimeLocalidade();
-	}
-	lista += fimLista;
-	divTela.innerHTML = lista;
-	
-}
-
-function imprimeNomes() {
-	var linha = "";
-	for (var i = 0; i < tribosNome.length; i++) {
-		linha += "<li><h4>" + tribosNome[i].nome + "</h4></li>";
-	} 
-	return linha;
-}
-
-function imprimeLingua() {
-	var linha = "";
-	for (var i = 0; i < tribosLingua.length; i++) {
-		linha += "<li><h4>" + tribosLingua[i].familiaLinguistica + "</h4></li>";
-	}
-	return linha;
-}
-
-function imprimeLocalidade() {
-	var linha = "";
-	for (var i = 0; i < tribosLocalidade.length; i++) {
-		linha += "<li><h4>" + tribosLocalidade[i].localidade + "</h4></li>";
-	}
-	return linha;
-}
-
-//Dados de tribos indígenas
-
-const tribosNome = [
-	{ "nome": "Aikanã"},
-	{ "nome": "Aikewara"},
-	{ "nome": "Akuntsu"},
-	{ "nome": "Amanayé"},
-	{ "nome": "Amondawa"},
-];
-
-const tribosLingua = [
-	{ "familiaLinguistica": "Aikaná"},
-	{ "familiaLinguistica": "Arawá"},
-	{ "familiaLinguistica": "Arikén"},
-	{ "familiaLinguistica": "Aruak"},
-	{ "familiaLinguistica": "Bora"},
-];
-
-const tribosLocalidade = [
-	{ "localidade": "Acre" },
-	{ "localidade": "Alagoas" },
-	{ "localidade": "Amazonas" },
-	{ "localidade": "Amapá" },
-	{ "localidade": "Bahia" },
-];
