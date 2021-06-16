b5dd460a54a5a45084a45d209476495600c4901e
Alex <alexpantolfo619@gmail.com>
2021-06-15 21:49:24 -0300

    Barra de busca

--- src/js/busca.js
+++ src/js/busca.js
@@ -4,7 +4,7 @@ let searchString = '';
 const searchBar = document.getElementById('search-bar');
 
 async function loadtribes() {
-    const res = await fetch('https://wiki.previa.app/api/search/');
+    const res = await fetch('https://wiki.previa.app/api/search/?fl=name,localizacao,paragrafo,familiaLinguistica,slug&rows=999');
     const tribos = await res.json();
     s = tribos.docs;
     console.log(s);
@@ -15,42 +15,58 @@ searchBar.addEventListener('keyup', (e) => {
     var searchString = (e.target.value.toLowerCase());
     var filteredtribes = s.filter(valortribo => {
         return (
-            valortribo.name.toLowerCase().includes(searchString) ||
-            valortribo.localizacao.toLowerCase().includes(searchString) ||
-            valortribo.familiaLinguistica.toLowerCase().includes(searchString)
+            ((valortribo.hasOwnProperty('familiaLinguistica')) ? valortribo.localizacao.toLowerCase().includes(searchString) : false) ||
+            ((valortribo.hasOwnProperty('localizacao')) ? valortribo.localizacao.toLowerCase().includes(searchString) : false) ||
+            valortribo.name.toLowerCase().includes(searchString)
         );
     });
     var tamanho = filteredtribes.length;
 
     console.log(filteredtribes);
 
-    while (searchString != '' || searchString == '') {
-        if (searchString != '' && tamanho != 0) {
-            var resul =
-                `<h1>${searchString}</h1>
+    if (searchString != '' && tamanho != 0) {
+        let test = `
+            <div id="resultadosencontrados">
+            </div>
+    
+            <div id="resultadobusca" class="container distanciamento">
+            </div>`;
+        var resul =
+            `<h1>${e.target.value}</h1>
         <p>${tamanho} resultados encontrados</p>`
-            let tpl = '';
-            filteredtribes.forEach(tribo => {
-                tpl +=
-                    `<div class="container">
-        <h3><a href="tribos/maxakali.html">${tribo.name}</a></h3>
-    ${tribo.paragrafo}
-    </div>`
-            });
-            document.getElementById('busca').style.display = 'none';
-            document.getElementById('main-image').style.display = 'none';
-            document.getElementById('resultadosencontrados').innerHTML = resul;
-            document.getElementById('resultadobusca').innerHTML = tpl;
-        } else if (searchString == '') {
-            let test = `<h1>Nada foi encontrado</h1>`
-            document.getElementById('test').style.display = 'none';
-            document.getElementById('test').innerHTML = test;
-            document.getElementById('busca').style.display = 'block';
-            document.getElementById('main-image').style.display = 'block';
-        } else if (tamanho == 0) {
-            let test = `<h1>Nada foi encontrado</h1>`
-            document.getElementById('test').innerHTML = test;
-        }
-        break;
+        let tpl = '';
+        filteredtribes.forEach(tribo => {
+
+            tpl +=
+                `<div class="container distanciamento">
+                 <h3><a href=#>${tribo.name}</a></h3>
+             ${tribo.paragrafo}
+             </div>`
+        });
+        document.getElementById('test').innerHTML = test;
+        document.getElementById('test').style.display = 'block';
+        document.getElementById('busca').style.display = 'none';
+        document.getElementById('main-image').style.display = 'none';
+        document.getElementById('resultadosencontrados').innerHTML = resul;
+        document.getElementById('resultadobusca').innerHTML = tpl;
+    } else if (searchString == '') {
+        let test = `<h1>Nada foi encontrado</h1>
+            <div id="resultadosencontrados">
+            </div>
+    
+            <div id="resultadobusca" class="container distanciamento">
+            </div>`
+        document.getElementById('test').style.display = 'none';
+        document.getElementById('test').innerHTML = test;
+        document.getElementById('busca').style.display = 'block';
+        document.getElementById('main-image').style.display = 'block';
+    } else if (tamanho == 0) {
+        let test = `<h1>Nada foi encontrado</h1>
+            <div id="resultadosencontrados">
+        </div>
+
+        <div id="resultadobusca" class="container distanciamento">
+        </div>`
+        document.getElementById('test').innerHTML = test;
     }
 });
