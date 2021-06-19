83f4582a8144b287f41550b5683e27da9b8ccca2
lianeml <elianemoraes.lessa@gmail.com>
2021-06-18 21:11:36 -0300

    versao basica do script para preencher pagina das tribos

--- src/js/tribos.js
+++ src/js/tribos.js
@@ -1,10 +1,31 @@
+
+
+// TODO: json para mapear sigla de estado para seu nome completo
+/*const stateName = {
+  "MG": "Minas Gerais",
+  "SP": "São Paulo",
+  //...
+}*/
+
 document.addEventListener('DOMContentLoaded', () => {
 
+  const tribo = (new URL(location.href)).searchParams.get("name");
+  const queryUrl = `https://wiki.previa.app/api/search/?q=slug:${tribo}&fq=module:comunidade`
 
-  // `tribos/index.html?name=${tribo}`
+  fetch(queryUrl, {
+    method: 'GET',
+  }).then(r => r.json())
+  .then(r => {
+    let info = r['docs'][0];
 
-  let tribo = (new URL(location.href)).searchParams.get("name");
+    document.getElementById('nomeTribo').innerHTML          = info['name'];
+    document.getElementById('paragrafo').innerHTML          = info['paragrafo'];
+    document.getElementById('autodenominacao').innerHTML    = info['autodenominacao'];
+    document.getElementById('localizacao').innerHTML        = info['localizacao'];
+    document.getElementById('populacao').innerHTML          = info['populacao'];
+    document.getElementById('familiaLinguistica').innerHTML = info['familiaLinguistica'];
 
-  console.log(tribo);
+  })
+
 
 })
