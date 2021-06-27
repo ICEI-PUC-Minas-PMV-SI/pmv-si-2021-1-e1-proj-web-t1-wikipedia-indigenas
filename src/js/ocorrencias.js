b4e080e387e00664c8ae74c86338dce2a4044ee5
Eric Mantoani <eu@eric.com.br>
2021-06-27 15:58:33 -0300

    ajustes na página de ocorrências

--- src/js/ocorrencias.js
+++ src/js/ocorrencias.js
@@ -1,4 +1,13 @@
+const _date = a => {
+  let Y = a.slice(0,4);
+  let m = a.slice(5,7);
+  let d = a.slice(8,10);
 
+  let H = a.slice(11,13);
+  let i = a.slice(14,16);
+
+  return `${d}/${m}/${Y} às ${H}h${i}`;
+}
 
 let ocorrencias = () => {
   fetch('https://wiki.previa.app/api/search/?fq=module:ocorrencias&sort=sortDate DESC&rows=999')
@@ -8,10 +17,9 @@ let ocorrencias = () => {
       let tinqs = '';
 
       r.docs.forEach(a => {
-        console.log(a);
-        tinqs += `<p id="dataHora">${a.sortDate}</p>
-        <p id="tribo">${a.name}</p>
-        <p id="fatos">${a.description}</p>`
+        tinqs += `<p class="dataHora">${_date(a.sortDate)}</p>
+        <p class="tribo">${a.name}</p>
+        <p class="fatos">${a.description}</p>`
       });
 
       document.getElementById('dadosOcorrencia').innerHTML += tinqs;
