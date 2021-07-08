d4ab99910006b951a06a77086cba8442e02aabe8
Alex <alexpantolfo619@gmail.com>
2021-07-08 11:00:11 -0300

    Arrumando links

--- src/js/home.js
+++ src/js/home.js
@@ -1,166 +1,166 @@
 let init = () => {
-  window.comunidades = JSON.parse(localStorage.getItem('comunidades'));
+    window.comunidades = JSON.parse(localStorage.getItem('comunidades'));
 
-  i18n = {
-    "AC": "Acre",
-    "AL": "Alagoas",
-    "AP": "Amapá",
-    "AM": "Amazonas",
-    "BA": "Bahia",
-    "CE": "Ceará",
-    "DF": "Distrito Federal",
-    "ES": "Espírito Santo",
-    "GO": "Goiás",
-    "MA": "Maranhão",
-    "MT": "Mato Grosso",
-    "MS": "Mato Grosso do Sul",
-    "MG": "Minas Gerais",
-    "PA": "Pará",
-    "PB": "Paraíba",
-    "PR": "Paraná",
-    "PE": "Pernambuco",
-    "PI": "Piauí",
-    "RJ": "Rio de Janeiro",
-    "RN": "Rio Grande do Norte",
-    "RS": "Rio Grande do Sul",
-    "RO": "Rondônia",
-    "RR": "Roraima",
-    "SC": "Santa Catarina",
-    "SP": "São Paulo",
-    "SE": "Sergipe",
-    "TO": "Tocantins",
-    "GF": "Guiana Francesa"
-  }
-
-  window.letras = {}
-  window.familiaLinguistica = {}
-  window.localidades = {}
-
-  comunidades.forEach(a => {
-    
-    if (a.slug != undefined) {
-      // fetch letras
-      let l = a.slug.slice(0, 1).toUpperCase();
-      if (!letras.hasOwnProperty(l)) {
-          letras[l] = [];
-      }
-      letras[l].push(a);
-
-      // fetch familiaLinguistica
-      if(a.familiaLinguistica != ''){
-        let f = a.familiaLinguistica;
-        if (!familiaLinguistica.hasOwnProperty(f)) {
-            familiaLinguistica[f] = [];
-        }
-        familiaLinguistica[f].push(a);  
-      }
-
-      // fetch localidade
-      if(a.localizacao != ''){
-        let g = a.localizacao;
-
-        g.split(',').forEach(b => {
-          b = b.trim()
-
-          if(b.indexOf('Guiana Francesa') > 0){
-            if (!localidades.hasOwnProperty('GF')) {
-              localidades.GF = [];
-            }
-            localidades.GF.push(a);
-            b = b.replace("Guiana Francesa", "").replace("  ", " ").trim();
-          }
-
-          b.split(" ").forEach(c => {
-            // console.log(c);
-            if(!i18n.hasOwnProperty(c)){
-              // console.log(c);
-              if (!localidades.hasOwnProperty(c)) {
-                localidades[c] = [];
-              }
-              localidades[c].push(a);
-
-              b = b.replace(c, '').trim();
-            }
-          });
-
-          if(b.indexOf('/') > 0){
-            b.split('/').forEach(d => {
-              if (!localidades.hasOwnProperty(d)) {
-                localidades[d] = [];
-              }
-              localidades[d].push(a);
-            })  
-          }
-          
-
-          // console.log(b);
-
-          if(b != ""){
-            if (!localidades.hasOwnProperty(b)) {
-              localidades[b] = [];
-            }
-            localidades[b].push(a);  
-          }
-          
-        }) 
-      }
-      // update i18n
-      i18n[a.slug] = a.name;
+    i18n = {
+        "AC": "Acre",
+        "AL": "Alagoas",
+        "AP": "Amapá",
+        "AM": "Amazonas",
+        "BA": "Bahia",
+        "CE": "Ceará",
+        "DF": "Distrito Federal",
+        "ES": "Espírito Santo",
+        "GO": "Goiás",
+        "MA": "Maranhão",
+        "MT": "Mato Grosso",
+        "MS": "Mato Grosso do Sul",
+        "MG": "Minas Gerais",
+        "PA": "Pará",
+        "PB": "Paraíba",
+        "PR": "Paraná",
+        "PE": "Pernambuco",
+        "PI": "Piauí",
+        "RJ": "Rio de Janeiro",
+        "RN": "Rio Grande do Norte",
+        "RS": "Rio Grande do Sul",
+        "RO": "Rondônia",
+        "RR": "Roraima",
+        "SC": "Santa Catarina",
+        "SP": "São Paulo",
+        "SE": "Sergipe",
+        "TO": "Tocantins",
+        "GF": "Guiana Francesa"
     }
-  })
+
+    window.letras = {}
+    window.familiaLinguistica = {}
+    window.localidades = {}
+
+    comunidades.forEach(a => {
+
+        if (a.slug != undefined) {
+            // fetch letras
+            let l = a.slug.slice(0, 1).toUpperCase();
+            if (!letras.hasOwnProperty(l)) {
+                letras[l] = [];
+            }
+            letras[l].push(a);
+
+            // fetch familiaLinguistica
+            if (a.familiaLinguistica != '') {
+                let f = a.familiaLinguistica;
+                if (!familiaLinguistica.hasOwnProperty(f)) {
+                    familiaLinguistica[f] = [];
+                }
+                familiaLinguistica[f].push(a);
+            }
+
+            // fetch localidade
+            if (a.localizacao != '') {
+                let g = a.localizacao;
+
+                g.split(',').forEach(b => {
+                    b = b.trim()
+
+                    if (b.indexOf('Guiana Francesa') > 0) {
+                        if (!localidades.hasOwnProperty('GF')) {
+                            localidades.GF = [];
+                        }
+                        localidades.GF.push(a);
+                        b = b.replace("Guiana Francesa", "").replace("  ", " ").trim();
+                    }
+
+                    b.split(" ").forEach(c => {
+                        // console.log(c);
+                        if (!i18n.hasOwnProperty(c)) {
+                            // console.log(c);
+                            if (!localidades.hasOwnProperty(c)) {
+                                localidades[c] = [];
+                            }
+                            localidades[c].push(a);
+
+                            b = b.replace(c, '').trim();
+                        }
+                    });
+
+                    if (b.indexOf('/') > 0) {
+                        b.split('/').forEach(d => {
+                            if (!localidades.hasOwnProperty(d)) {
+                                localidades[d] = [];
+                            }
+                            localidades[d].push(a);
+                        })
+                    }
+
+
+                    // console.log(b);
+
+                    if (b != "") {
+                        if (!localidades.hasOwnProperty(b)) {
+                            localidades[b] = [];
+                        }
+                        localidades[b].push(a);
+                    }
+
+                })
+            }
+            // update i18n
+            i18n[a.slug] = a.name;
+        }
+    })
 }
 
 
 
 const Home = {
-  init: () => {
-    Home.cards();
-    Home.ocorrencias();
-  },
+    init: () => {
+        Home.cards();
+        Home.ocorrencias();
+    },
 
-  ocorrencias: () => {
-    fetch('https://wiki.previa.app/api/search/?fq=module:ocorrencias&sort=sortDate DESC&rows=3')
-      .then(r => r.json())
-      .then(r => {
-        document.getElementById('ocorrenciasNumFound').innerText = r.numFound;
+    ocorrencias: () => {
+        fetch('https://wiki.previa.app/api/search/?fq=module:ocorrencias&sort=sortDate DESC&rows=3')
+            .then(r => r.json())
+            .then(r => {
+                document.getElementById('ocorrenciasNumFound').innerText = r.numFound;
 
-        let tpl = '<table>';
+                let tpl = '<table>';
 
-        r.docs.forEach(a => {
-          if(a.name != '' && a.name != 'undefined'){
+                r.docs.forEach(a => {
+                    if (a.name != '' && a.name != 'undefined') {
 
-            let description = a.description;
-            if(description.length > 50){
-              description = description.slice(0, 50) + '...';
-            }
+                        let description = a.description;
+                        if (description.length > 50) {
+                            description = description.slice(0, 50) + '...';
+                        }
 
-            tpl += 
-              `<tr>
+                        tpl +=
+                            `<tr>
                 <td style="padding-right:1rem">${i18n[a.name]}</td>
                 <td>${description}</td>
-              </tr>` 
-          }
-        })
-        tpl += `</table>`
+              </tr>`
+                    }
+                })
+                tpl += `</table>`
 
-        document.getElementById('ocorrenciasLast').innerHTML = tpl;
+                document.getElementById('ocorrenciasLast').innerHTML = tpl;
 
-      });
-  },
+            });
+    },
 
-  cards: () => {
-    let randomCard1 = comunidades[Math.floor(Math.random() * comunidades.length)];
-    let randomCard2 = comunidades[Math.floor(Math.random() * comunidades.length)];
-    let randomCard3 = comunidades[Math.floor(Math.random() * comunidades.length)];
+    cards: () => {
+        let randomCard1 = comunidades[Math.floor(Math.random() * comunidades.length)];
+        let randomCard2 = comunidades[Math.floor(Math.random() * comunidades.length)];
+        let randomCard3 = comunidades[Math.floor(Math.random() * comunidades.length)];
 
-    while (randomCard1 == randomCard2 || randomCard2 == randomCard3 || randomCard1 == randomCard3 || randomCard1.imagem == '' || randomCard1.paragrafo == '' || randomCard2.imagem == '' || randomCard2.paragrafo == '' || randomCard3.imagem == '' || randomCard3.paragrafo == '') {
-        randomCard1 = comunidades[Math.floor(Math.random() * comunidades.length)];
-        randomCard2 = comunidades[Math.floor(Math.random() * comunidades.length)];
-        randomCard3 = comunidades[Math.floor(Math.random() * comunidades.length)];
-    }
+        while (randomCard1 == randomCard2 || randomCard2 == randomCard3 || randomCard1 == randomCard3 || randomCard1.imagem == '' || randomCard1.paragrafo == '' || randomCard2.imagem == '' || randomCard2.paragrafo == '' || randomCard3.imagem == '' || randomCard3.paragrafo == '') {
+            randomCard1 = comunidades[Math.floor(Math.random() * comunidades.length)];
+            randomCard2 = comunidades[Math.floor(Math.random() * comunidades.length)];
+            randomCard3 = comunidades[Math.floor(Math.random() * comunidades.length)];
+        }
 
-    const card1 =
-        `<a href="../tribos.html?name=${randomCard1.slug}">
+        const card1 =
+            `<a href="./tribos.html?name=${randomCard1.slug}">
          <div class="card">
            <img class="card-img-top" src="${randomCard1.imagem}">
            <div class="card-body">
@@ -169,9 +169,9 @@ const Home = {
            </div>
          </div>
          </a>`
-    document.getElementById('card1').innerHTML = card1;
+        document.getElementById('card1').innerHTML = card1;
 
-    const card2 = `<a href=../tribos.html?name=${randomCard2.slug}>
+        const card2 = `<a href=./tribos.html?name=${randomCard2.slug}>
     <div class="card">
       <img class="card-img-top" src="${randomCard2.imagem}">
       <div class="card-body">
@@ -180,9 +180,9 @@ const Home = {
       </div>
     </div>
     </a>`
-    document.getElementById('card2').innerHTML = card2;
+        document.getElementById('card2').innerHTML = card2;
 
-    const card3 = `<a href=../tribos.html?name=${randomCard3.slug}>
+        const card3 = `<a href=./tribos.html?name=${randomCard3.slug}>
     <div class="card">
     <img class="card-img-top" src="${randomCard3.imagem}">
     <div class="card-body">
@@ -191,8 +191,8 @@ const Home = {
     </div>
     </div>
     </a>`
-    document.getElementById('card3').innerHTML = card3;
-  }
+        document.getElementById('card3').innerHTML = card3;
+    }
 
 
 }
@@ -200,105 +200,105 @@ const Home = {
 
 const Indice = {
 
-  DOM: {
-    content: document.getElementById('tribos'),
-    buttons: document.querySelectorAll('#encontre .btn-group > button')
-  },
+    DOM: {
+        content: document.getElementById('tribos'),
+        buttons: document.querySelectorAll('#encontre .btn-group > button')
+    },
 
-  init: () => {
-    Indice.nome();
-    Indice.listen();
-  },
+    init: () => {
+        Indice.nome();
+        Indice.listen();
+    },
 
-  listen: () => {
-    Indice.DOM.buttons.forEach(a => {
-      a.addEventListener('click', function(e) {
-        let method = this.getAttribute('id');
-        Indice[method]();
-      }, false);
-    })
-  },
+    listen: () => {
+        Indice.DOM.buttons.forEach(a => {
+            a.addEventListener('click', function(e) {
+                let method = this.getAttribute('id');
+                Indice[method]();
+            }, false);
+        })
+    },
 
-  nome: () => {
-    let tpl = '';
+    nome: () => {
+        let tpl = '';
 
-    Object.keys(letras).sort().forEach(l => {
-      tpl += 
-        `<div class="letra">
+        Object.keys(letras).sort().forEach(l => {
+            tpl +=
+                `<div class="letra">
           <h2>${l}</h2>`;
 
-      letras[l].sort((a, b) => (a.name > b.name) ? 1 : -1).forEach(a => {
-        tpl += 
-        `<nav>
-          <a href="../tribos.html?name=${a.slug}">${a.name}</a>
+            letras[l].sort((a, b) => (a.name > b.name) ? 1 : -1).forEach(a => {
+                tpl +=
+                    `<nav>
+          <a href="./tribos.html?name=${a.slug}">${a.name}</a>
         </nav>`
-      })
+            })
 
-      tpl += `</div>`;
+            tpl += `</div>`;
 
-    });
+        });
 
-    Indice.DOM.content.innerHTML = tpl;
-  },
+        Indice.DOM.content.innerHTML = tpl;
+    },
 
-  familiaLinguistica: () => {
-    let tpl = '';
+    familiaLinguistica: () => {
+        let tpl = '';
 
-    Object.keys(familiaLinguistica).sort().forEach(l => {
-      tpl += 
-        `<div class="letra">
+        Object.keys(familiaLinguistica).sort().forEach(l => {
+            tpl +=
+                `<div class="letra">
           <h2 class="familia">${l}</h2>`;
 
-      familiaLinguistica[l].sort((a, b) => (a.name > b.name) ? 1 : -1).forEach(a => {
-        tpl += 
-        `<nav>
-          <a href="../tribos.html?name=${a.slug}">${a.name}</a>
+            familiaLinguistica[l].sort((a, b) => (a.name > b.name) ? 1 : -1).forEach(a => {
+                tpl +=
+                    `<nav>
+          <a href="./tribos.html?name=${a.slug}">${a.name}</a>
         </nav>`
-      })
+            })
 
-      tpl += `</div>`;
+            tpl += `</div>`;
 
-    });
+        });
 
-    Indice.DOM.content.innerHTML = tpl;
-  },
+        Indice.DOM.content.innerHTML = tpl;
+    },
 
-  localidade: () => {
-    let tpl = '';
+    localidade: () => {
+        let tpl = '';
 
-    Object.keys(localidades).sort().forEach(l => {
+        Object.keys(localidades).sort().forEach(l => {
 
-      let label = (i18n.hasOwnProperty(l)) ? i18n[l] : l ;
+            let label = (i18n.hasOwnProperty(l)) ? i18n[l] : l;
 
-      tpl += 
-        `<div class="letra">
+            tpl +=
+                `<div class="letra">
           <h2 class="familia">${label}</h2>`;
 
-      localidades[l].sort((a, b) => (a.name > b.name) ? 1 : -1).forEach(a => {
-        tpl += 
-        `<nav>
-          <a href="../tribos.html?name=${a.slug}">${a.name}</a>
+            localidades[l].sort((a, b) => (a.name > b.name) ? 1 : -1).forEach(a => {
+                tpl +=
+                    `<nav>
+          <a href="./tribos.html?name=${a.slug}">${a.name}</a>
         </nav>`
-      })
+            })
 
-      tpl += `</div>`;
+            tpl += `</div>`;
 
-    });
+        });
 
-    Indice.DOM.content.innerHTML = tpl;
-  }
+        Indice.DOM.content.innerHTML = tpl;
+    }
 }
 
 let interval = setInterval(() => {
-  if(!loading){
-    init();
-    Home.init();
-    Indice.init();
+    if (!loading) {
+        init();
+        Home.init();
+        Indice.init();
 
-    clearInterval(interval);
-  }
+        clearInterval(interval);
+    }
 }, 200)
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+
 });
--- src/js/tribos.js
+++ src/js/tribos.js
@@ -1,67 +1,63 @@
-
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
 
-  const tribo = (new URL(location.href)).searchParams.get("name");
-  const queryUrl = `https://wiki.previa.app/api/search/?q=slug:${tribo}&fq=module:comunidade`
+    const tribo = (new URL(location.href)).searchParams.get("name");
+    const queryUrl = `https://wiki.previa.app/api/search/?q=slug:${tribo}&fq=module:comunidade`
 
-  fetch(queryUrl, {
-    method: 'GET',
-  }).then(r => r.json())
-  .then(r => {
-    let info = r['docs'][0];
+    fetch(queryUrl, {
+            method: 'GET',
+        }).then(r => r.json())
+        .then(r => {
+            let info = r['docs'][0];
 
-    document.getElementById('tituloTribo').innerHTML          = info['name'];
-    document.getElementById('nomeTribo').innerHTML          = info['name'];
-    document.getElementById('paragrafo').innerHTML          = info['paragrafo'];
-    document.getElementById('autodenominacao').innerHTML    = info['autodenominacao'];
-    document.getElementById('localizacao').innerHTML        = info['localizacao'];
-    document.getElementById('populacao').innerHTML          = info['populacao'];
-    document.getElementById('familiaLinguistica').innerHTML = info['familiaLinguistica'];
+            document.getElementById('tituloTribo').innerHTML = info['name'];
+            document.getElementById('nomeTribo').innerHTML = info['name'];
+            document.getElementById('paragrafo').innerHTML = info['paragrafo'];
+            document.getElementById('autodenominacao').innerHTML = info['autodenominacao'];
+            document.getElementById('localizacao').innerHTML = info['localizacao'];
+            document.getElementById('populacao').innerHTML = info['populacao'];
+            document.getElementById('familiaLinguistica').innerHTML = info['familiaLinguistica'];
 
-    document.getElementById('main-image').style.backgroundImage = `url('${info.imagem}')`;
+            document.getElementById('main-image').style.backgroundImage = `url('${info.imagem}')`;
 
-    document.getElementsByTagName('title')[0].innerText = `${info['name']} - Wiki Indígenas`
-    
-    if(info.hasOwnProperty('link') && info.link != '' ){
-      document.getElementById('readMore').innerHTML = 
-        `<a id="readMore" href="${info.link}" target="_blank">Leia mais...</a>`
-    }
-    
-    document.querySelector('a[name="contato"]')
-      .setAttribute('href', `../../contato-com-a-comunidade.html?name=${info.slug}`);
+            document.getElementsByTagName('title')[0].innerText = `${info['name']} - Wiki Indígenas`
 
-    document.querySelector('a[name="cadastre-se"]')
-      .setAttribute('href', `../../cadastre-se-como-contato.html?name=${info.slug}`);
+            if (info.hasOwnProperty('link') && info.link != '') {
+                document.getElementById('readMore').innerHTML =
+                    `<a id="readMore" href="${info.link}" target="_blank">Leia mais...</a>`
+            }
 
-    if(info.hasOwnProperty('nomes')){
+            document.querySelector('a[name="contato"]')
+                .setAttribute('href', `contato-com-a-comunidade.html?name=${info.slug}`);
 
-      let autor = "";
-      info.nomes.forEach((v,k) => {
-        if(k > 0){
-          autor += "<br /> e ";
-        } else {
-          if(v.length > 0){
-            autor += "Por: ";
-          }
-        }
-        autor += v;
-        if(info.hasOwnProperty('cargos')){
-          if(info.cargos[k] != ''){
-            autor += `<br />${info.cargos[k]}`
-          }  
-        }
-      })
+            document.querySelector('a[name="cadastre-se"]')
+                .setAttribute('href', `cadastre-se-como-contato.html?name=${info.slug}`);
 
-      document.getElementById('autor').innerHTML = autor;
-      document.getElementById('original').innerHTML = `<a href=${info.link}>ver original <i class="fas fa-external-link-alt"></i></a>`;
+            if (info.hasOwnProperty('nomes')) {
 
-    }
-    
+                let autor = "";
+                info.nomes.forEach((v, k) => {
+                    if (k > 0) {
+                        autor += "<br /> e ";
+                    } else {
+                        if (v.length > 0) {
+                            autor += "Por: ";
+                        }
+                    }
+                    autor += v;
+                    if (info.hasOwnProperty('cargos')) {
+                        if (info.cargos[k] != '') {
+                            autor += `<br />${info.cargos[k]}`
+                        }
+                    }
+                })
 
-  })
+                document.getElementById('autor').innerHTML = autor;
+                document.getElementById('original').innerHTML = `<a href=${info.link}>ver original <i class="fas fa-external-link-alt"></i></a>`;
+
+            }
+
+
+        })
 
 
 })
